d0323f6482e060acc35a0f9b9aa6a996c1f4eabc
Stefan Dörig <sdoerig@bluewin.ch>
2021-03-13 18:58:34 +0100

    My rustlings solutions

---
+++ exercises/clippy/target/debug/deps/clippy2-fa81292bdc7e4a2b.d
@@ -0,0 +1,5 @@
+/home/doerig/rustlings/exercises/clippy/target/debug/deps/clippy2-fa81292bdc7e4a2b.rmeta: clippy2.rs
+
+/home/doerig/rustlings/exercises/clippy/target/debug/deps/clippy2-fa81292bdc7e4a2b.d: clippy2.rs
+
+clippy2.rs:
---
+++ exercises/enums/enums3.rs
@@ -0,0 +1,72 @@
+// enums3.rs
+// Address all the TODOs to make the tests pass!
+
+
+
+enum Message {
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit
+}
+
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+struct State {
+    color: (u8, u8, u8),
+    position: Point,
+    quit: bool,
+}
+
+impl State {
+    fn change_color(&mut self, color: (u8, u8, u8)) {
+        self.color = color;
+    }
+
+    fn quit(&mut self) {
+        self.quit = true;
+    }
+
+    fn echo(&self, s: String) {
+        println!("{}", s);
+    }
+
+    fn move_position(&mut self, p: Point) {
+        self.position = p;
+    }
+
+    fn process(&mut self, message: Message) {
+        match message {
+            Message::ChangeColor(t) => self.color = (t.0,t.1,t.2),
+            Message::Move(p) => self.position = Point{x: p.x, y: p.y},
+            Message::Quit => self.quit(),
+            Message::Echo(s) => println!("{}", s)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_message_call() {
+        let mut state = State {
+            quit: false,
+            position: Point { x: 0, y: 0 },
+            color: (0, 0, 0),
+        };
+        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::Echo(String::from("hello world")));
+        state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Quit);
+
+        assert_eq!(state.color, (255, 0, 255));
+        assert_eq!(state.position.x, 10);
+        assert_eq!(state.position.y, 15);
+        assert_eq!(state.quit, true);
+    }
+}
